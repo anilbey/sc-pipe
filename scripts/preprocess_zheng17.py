7f3fd5d685712c7c82967f06913874793cb89b0d
Mustafa Anil Tuncel <mtuncel@lo-login-02.leonhard.ethz.ch>
2018-01-30 17:05:22 +0100

    simlr & phenograph included

--- scripts/preprocess_zheng17.py
+++ scripts/preprocess_zheng17.py
@@ -3,7 +3,7 @@ import scanpy.api as sc
 
 
 
-def preprocess_zheng17(loom_file, out_path, threads):
+def preprocess_zheng17(loom_file, out_path, transpose, threads):
     ds = loompy.connect(loom_file)
     # Scanpy filtering functions assume that: genes=variables, Transpose the matrix
     adata = sc.AnnData(ds[:,:].T, obs=ds.col_attrs, var=ds.row_attrs)
@@ -14,11 +14,19 @@ def preprocess_zheng17(loom_file, out_path, threads):
     adata = adata[:, filter_result.gene_subset]
 
     # Writing the output loom file
-    matrix = adata.X.T
     gene_names = adata.var['genes'].values
     cell_names = adata.obs['cells'].values
-    col_attrs = { "cells": cell_names }
-    row_attrs = { "genes": gene_names }
+        
+    if not transpose:
+        matrix = adata.X.T
+        col_attrs = { "cells": cell_names }
+        row_attrs = { "genes": gene_names }
+    else:
+        matrix = adata.X
+        row_attrs = {"cells": cell_names}
+        col_attrs = {"genes": gene_names}
+    print(matrix.shape)
     loompy.create(out_path, matrix=matrix, row_attrs=row_attrs, col_attrs=col_attrs)
     
-preprocess_zheng17(snakemake.input.loom_file.__str__(), snakemake.output.__str__(), snakemake.threads)
+preprocess_zheng17(snakemake.input.loom_file.__str__(),
+        snakemake.output.__str__(), snakemake.params.transpose,  snakemake.threads)
--- snakefile_benchmark_clust.py
+++ snakefile_benchmark_clust.py
@@ -1,5 +1,5 @@
 configfile: 'config/config.json'
-SAMPLE = 'melanomaS2'
+SAMPLE = ['melanomaS2']
 HDF5_OUTPUT = 'hdf5_data'
 SIMULATED_DATA_OUTPUT = 'simulated/dropout_present'
 ANALYSIS_OUTPUT = 'analysis/dropout_present'
@@ -11,8 +11,8 @@ rules
 
 rule all:
     input:
-        expand(ANALYSIS_OUTPUT+'/{method}/'+'clusters/'+SAMPLE+'_sim_loc'+'{loc}'+'clusters.csv',
-                loc=config['splat_simulate']['de_loc_factor'],method=config['dim_reduction']['methods_used'])
+        expand(ANALYSIS_OUTPUT+'/{method}/'+'clusters/{sample}_sim_loc'+'{loc}'+'clusters.csv',
+                loc=config['splat_simulate']['de_loc_factor'],method=config['dim_reduction']['methods_used'],sample=SAMPLE)
     shell:
         'echo test rule all {input}'
 
@@ -21,42 +21,42 @@ rule all:
 
 rule cluster_results:
     input:
-        ANALYSIS_OUTPUT+'/{method}/'+SAMPLE+'_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/{method}/{sample}_sim_loc'+'{loc}'+'.csv'
     params:
         n_clusters = len(config['splat_simulate']['group_prob'])
     output:
-        ANALYSIS_OUTPUT+'/{method}/'+'clusters/'+SAMPLE+'_sim_loc'+'{loc}'+'clusters.csv'
+        ANALYSIS_OUTPUT+'/{method}/'+'clusters/{sample}_sim_loc'+'{loc}'+'clusters.csv'
     script:
         "scripts/k-means.py"
 
 rule create_loom:
     input:
-        genes_file = 'cell_ranger_output/'+SAMPLE+'/outs/filtered_gene_bc_matrices/GRCh38/genes.tsv',
-        matrix_file = 'cell_ranger_output/'+SAMPLE+'/outs/filtered_gene_bc_matrices/GRCh38/matrix.mtx',
-        barcodes_file = 'cell_ranger_output/'+SAMPLE+'/outs/filtered_gene_bc_matrices/GRCh38/barcodes.tsv'
+        genes_file = 'cell_ranger_output/{sample}/outs/filtered_gene_bc_matrices/GRCh38/genes.tsv',
+        matrix_file = 'cell_ranger_output/{sample}/outs/filtered_gene_bc_matrices/GRCh38/matrix.mtx',
+        barcodes_file = 'cell_ranger_output/{sample}/outs/filtered_gene_bc_matrices/GRCh38/barcodes.tsv'
     output:
-        HDF5_OUTPUT+'/'+SAMPLE+'.loom'
+        HDF5_OUTPUT+'/{sample}.loom'
     script:
         "scripts/create_loom.py"
 
 
 rule preprocess_zheng17:
     input:
-        loom_file = HDF5_OUTPUT+'/'+SAMPLE+'.loom'	
+        loom_file = HDF5_OUTPUT+'/{sample}.loom'	
     output:
-        HDF5_OUTPUT+'/'+SAMPLE+'_zheng17.loom'
+        HDF5_OUTPUT+'/{sample}_zheng17.loom'
     script: 
         "scripts/preprocess_zheng17.py"
 
 # parallel
 rule simulate_data:
     input:
-        sample_loom = HDF5_OUTPUT+'/'+SAMPLE+'_zheng17.loom'
+        sample_loom = HDF5_OUTPUT+'/{sample}_zheng17.loom'
     params:
         group_prob = config['splat_simulate']['group_prob'],
         dropout_present = config['splat_simulate']['dropout_present']
     output:
-        SIMULATED_DATA_OUTPUT+'/'+SAMPLE+'_sim_loc'+'{loc}'+'.loom'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
     script:
         "scripts/data_simulation.R"
 
@@ -65,52 +65,73 @@ rule simulate_data:
 
 rule pca:
     input:
-        SIMULATED_DATA_OUTPUT+'/'+SAMPLE+'_sim_loc'+'{loc}'+'.loom'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
     params:
         n_components = config['dim_reduction']['pca']['n_components']
     output:
-        ANALYSIS_OUTPUT+'/pca/'+SAMPLE+'_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/pca/{sample}_sim_loc'+'{loc}'+'.csv'
     script:
         "scripts/pca.py"
+
+rule simlr:
+    input:
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
+    params:
+        n_components = config['dim_reduction']['simlr']['n_components'],
+        pca_components = config['dim_reduction']['simlr']['pca_components'],
+        n_neighbours = config['dim_reduction']['simlr']['n_neighbours'],
+        max_iter = config['dim_reduction']['simlr']['max_iter']
+    output:
+        ANALYSIS_OUTPUT+'/simlr/{sample}_sim_loc'+'{loc}'+'.csv'
+    script:
+        "scripts/simlr_large.py"
 
 
 rule factor_analysis:
     input:
-        SIMULATED_DATA_OUTPUT+'/'+SAMPLE+'_sim_loc'+'{loc}'+'.loom'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
     params:
         n_components = config['dim_reduction']['factor_analysis']['n_components']
     output:
-        ANALYSIS_OUTPUT+'/factor_analysis/'+SAMPLE+'_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/factor_analysis/{sample}_sim_loc'+'{loc}'+'.csv'
     script:
         "scripts/factor_analysis.py"
 
 rule tsne:
     input:
-        SIMULATED_DATA_OUTPUT+'/'+SAMPLE+'_sim_loc'+'{loc}'+'.loom'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
     params:
         n_components = config['dim_reduction']['tsne']['n_components']
     output:
-        ANALYSIS_OUTPUT+'/tsne/'+SAMPLE+'_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/tsne/{sample}_sim_loc'+'{loc}'+'.csv'
     script:
         "scripts/tsne.py"
 
+rule phenograph:
+    input:
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
+    params:
+        n_neighbours = config['clustering']['phenograph']['n_neighbours']
+    output:
+        ANALYSIS_OUTPUT+'/phenograph/clusters/{sample}_sim_loc'+'{loc}'+'clusters.csv'
+    threads:8
+    script:
+        "scripts/pheno.py"
+
+
+
 rule zifa:
     input:
-        SIMULATED_DATA_OUTPUT+'/'+SAMPLE+'_sim_loc'+'{loc}'+'.loom'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc'+'{loc}'+'.loom'
     params:
         n_components = config['dim_reduction']['block_zifa']['n_components'],
         n_blocks = config['dim_reduction']['block_zifa']['n_blocks']
     output:
-        ANALYSIS_OUTPUT+'/block_zifa/'+SAMPLE+'_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/block_zifa/{sample}_sim_loc'+'{loc}'+'.csv'
     script:
         "scripts/zifa.py"
 
-'''
 
 
 
 
-rule simlr:
-
-
-'''
