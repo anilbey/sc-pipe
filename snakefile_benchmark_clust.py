b84d36cde2b0327045fb8a3f7d0895fe849bb2f7
Tuncel  Mustafa Anil <tuncel.manil@gmail.com>
2018-03-09 15:59:08 +0100

    simulation with various DE probs

--- snakefile_benchmark_clust.py
+++ snakefile_benchmark_clust.py
@@ -2,7 +2,7 @@ import glob
 import numpy as np
 
 configfile: 'config/config.json'
-SAMPLE = ['melanomaS2']  #['hgmm100']  
+SAMPLE = ['melanomaS2']  #['hgmm100']
 HDF5_OUTPUT = 'hdf5_data'
 SIMULATED_DATA_OUTPUT = config['simulated_data_output']
 ANALYSIS_OUTPUT = SIMULATED_DATA_OUTPUT+'/analysis'
@@ -19,17 +19,18 @@ rules
 def dirichlet_group_prob(size):
     dirr =  np.random.dirichlet(np.ones(size))
     return dirr
-    
+
 
 def get_all_fastqs(path):
     fastqs =  glob.glob(path+'/*.fastq.gz')
 
 rule all:
     input:
-        direct_clustering_results = expand(ANALYSIS_OUTPUT+'/{method}/'+'clusters/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv',de_prob= config['splat_simulate']['de_loc_factor'], loc=config['splat_simulate']['de_loc_factor'], method=config['clustering']['methods_used'], sample=SAMPLE),
+        direct_clustering_results = expand(ANALYSIS_OUTPUT+'/{method}/'+'clusters/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv',de_prob= config['splat_simulate']['de_prob'], \
+        loc=config['splat_simulate']['de_loc_factor'], method=config['clustering']['methods_used'], sample=SAMPLE),
 #        dim_red_results = expand(ANALYSIS_OUTPUT+'/{method}/{sample}_sim_loc{loc}.csv', loc=config['splat_simulate']['de_loc_factor'], method=config['dim_reduction']['methods_used'], sample=SAMPLE),
-        dim_red_clustering_results = expand(ANALYSIS_OUTPUT+'/silhouette-{method}/'+'clusters/{c_method}_{sample}_sim_loc{loc}.csv',
-                loc=config['splat_simulate']['de_loc_factor'], method=config['dim_reduction']['methods_used'], c_method=config['dim_reduction']['clustering_methods'], sample=SAMPLE)
+        dim_red_clustering_results = expand(ANALYSIS_OUTPUT+'/silhouette-{method}/'+'clusters/{c_method}_{sample}_sim_de{de_prob}_loc{loc}.csv', de_prob= config['splat_simulate']['de_prob'], \
+        loc=config['splat_simulate']['de_loc_factor'], method=config['dim_reduction']['methods_used'], c_method=config['dim_reduction']['clustering_methods'], sample=SAMPLE)
 #TODO input function        fastqs = get_all_fastqs(config['input_fastqs'])
     shell:
         'echo test rule all {input}'
@@ -44,7 +45,7 @@ rule all:
 rule cellranger_count: # (parallel)
     input:
         fastqs_dir = config['input_fastqs']+'/{sample}',
-        reference = config['reference_transcriptome'] 
+        reference = config['reference_transcriptome']
     params:
         id = config['unique_run_id']
     output:
@@ -95,7 +96,7 @@ rule simulate_data:
     input:
         sample_hdf5 = rules.estimate_params.output
     params:
-        group_prob = dirichlet_group_prob(config['splat_simulate']['group_size']),
+        group_prob = dirichlet_group_prob(config['splat_simulate']['group_sizes']),
         dropout_present = config['splat_simulate']['dropout_present']
     wildcard_constraints:
         loc="\d(\.\d+)?",
@@ -106,37 +107,38 @@ rule simulate_data:
         out = LOG_FILES+'/simulate_data/sample_{sample}_de{de_prob}_loc_{loc}.out',
         err = LOG_FILES+'/simulate_data/sample_{sample}_de{de_prob}_loc_{loc}.err'
     shell:
-        "Rscript scripts/data_simulation.R --input {input.sample_hdf5} --group_prob {params.group_prob} --dropout_present {params.dropout_present} --output {output} --loc {wildcards.loc} --de_prob {wildcards.de_prob} 2> {log.err} 1> {log.out}"
+        "Rscript scripts/data_simulation.R --input {input.sample_hdf5} --group_prob {params.group_prob} --dropout_present \
+         {params.dropout_present} --output {output} --loc {wildcards.loc} --de_prob {wildcards.de_prob} 2> {log.err} 1> {log.out}"
 
 
 rule preprocess_zheng17:
     '''
-    regex pattern 
+    regex pattern
         loc: 0.25, 0.5, 1, 1.4, 2
     '''
     input:
-        hdf5_file = rules.simulate_data.output 
+        hdf5_file = rules.simulate_data.output
     wildcard_constraints:
         loc="\d(\.\d+)?"
     params:
         n_top_genes = config['preprocess']['zheng17']['n_top_genes']
     output:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     log:
-        out = LOG_FILES+'/preprocess_zheng17/sample_{sample}_loc_{loc}.out',
-        err = LOG_FILES+'/preprocess_zheng17/sample_{sample}_loc_{loc}.err'
-    shell: 
+        out = LOG_FILES+'/preprocess_zheng17/sample_{sample}_de{de_prob}_loc_{loc}.out',
+        err = LOG_FILES+'/preprocess_zheng17/sample_{sample}_de{de_prob}_loc_{loc}.err'
+    shell:
         "python scripts/preprocess_zheng17.py -i {input.hdf5_file} -o {output} --n_top_genes {params.n_top_genes} 2> {log.err} 1> {log.out}"
 
 # silhouette rules run for each dimensionality reduction results
 rule silhouette_hierarchical:
     input:
-        ANALYSIS_OUTPUT+'/{method}/{sample}_sim_loc'+'{loc}'+'.csv'    
+        ANALYSIS_OUTPUT+'/{method}/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     output:
-        ANALYSIS_OUTPUT+'/silhouette-{method}/clusters/hierarchical_{sample}_sim_loc{loc}.csv'
+        ANALYSIS_OUTPUT+'/silhouette-{method}/clusters/hierarchical_{sample}_sim_de{de_prob}_loc{loc}.csv'
     log:
-        out = LOG_FILES+'/silhouette_hierarchical/{method}_sample_{sample}_loc_{loc}.out',
-        err = LOG_FILES+'/silhouette_hierarchical/{method}_sample_{sample}_loc_{loc}.err'
+        out = LOG_FILES+'/silhouette_hierarchical/{method}_sample_{sample}_de{de_prob}_loc_{loc}.out',
+        err = LOG_FILES+'/silhouette_hierarchical/{method}_sample_{sample}_de{de_prob}_loc_{loc}.err'
     params:
         affinity = config['clustering']['hierarchical']['affinity'],
         linkage = config['clustering']['hierarchical']['linkage'],
@@ -148,12 +150,12 @@ rule silhouette_hierarchical:
 
 rule silhouette_kmeans:
     input:
-        ANALYSIS_OUTPUT+'/{method}/{sample}_sim_loc'+'{loc}'+'.csv'    
+        ANALYSIS_OUTPUT+'/{method}/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     output:
-        ANALYSIS_OUTPUT+'/silhouette-{method}/clusters/kmeans_{sample}_sim_loc{loc}.csv'
+        ANALYSIS_OUTPUT+'/silhouette-{method}/clusters/kmeans_{sample}_sim_de{de_prob}_loc{loc}.csv'
     log:
-        out = LOG_FILES+'/silhouette_kmeans/{method}_sample_{sample}_loc_{loc}.out',
-        err = LOG_FILES+'/silhouette_kmeans/{method}_sample_{sample}_loc_{loc}.err'
+        out = LOG_FILES+'/silhouette_kmeans/{method}_sample_{sample}_de{de_prob}_loc_{loc}.out',
+        err = LOG_FILES+'/silhouette_kmeans/{method}_sample_{sample}_de{de_prob}_loc_{loc}.err'
     params:
         n_init = config['clustering']['kmeans']['n_init'],
         n_jobs = config['clustering']['kmeans']['n_jobs'],
@@ -165,20 +167,20 @@ rule silhouette_kmeans:
 
 rule pca:
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         n_components = config['dim_reduction']['pca']['n_components']
     output:
-        ANALYSIS_OUTPUT+'/pca/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/pca/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/pca/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/pca/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/pca/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/pca/sample_{sample}_de{de_prob}loc_{loc}.err'
     shell:
         "python scripts/apply_pca.py {input} {output} {params.n_components} 2> {log.err} 1> {log.out}"
 
 rule simlr:
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         n_components = config['clustering']['simlr']['n_components'],
         pca_components = config['clustering']['simlr']['pca_components'],
@@ -189,86 +191,82 @@ rule simlr:
         k_max = config['clustering']['silhouette']['k_max'],
         metric = config['clustering']['silhouette']['metric']
     output:
-        ANALYSIS_OUTPUT+'/simlr/clusters/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/simlr/clusters/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/simlr/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/simlr/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/simlr/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/simlr/sample_{sample}_de{de_prob}loc_{loc}.err'
     shell:
-        "python scripts/apply_silhouette_simlr.py --input {input} --output {output} --n_components {params.n_components} --pca_components {params.pca_components} --n_neighbours {params.n_neighbours} --max_iter {params.max_iter} --k_min {params.k_min} --k_max {params.k_max} --metric {params.metric}  2> {log.err} 1> {log.out}"
+        "python scripts/apply_silhouette_simlr.py --input {input} --output {output} --n_components {params.n_components} --pca_components {params.pca_components} \
+         --n_neighbours {params.n_neighbours} --max_iter {params.max_iter} --k_min {params.k_min} --k_max {params.k_max} --metric {params.metric}  2> {log.err} 1> {log.out}"
 
 
 rule factor_analysis:
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         n_components = config['dim_reduction']['factor_analysis']['n_components']
     output:
-        ANALYSIS_OUTPUT+'/factor_analysis/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/factor_analysis/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/factor_analysis/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/factor_analysis/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/factor_analysis/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/factor_analysis/sample_{sample}_de{de_prob}loc_{loc}.err'
     shell:
         "python scripts/apply_fa.py {input} {output} {params.n_components} 2> {log.err} 1> {log.out} "
 
 rule tsne:
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         n_components = config['dim_reduction']['tsne']['n_components'],
         init = config['dim_reduction']['tsne']['init']
     output:
-        ANALYSIS_OUTPUT+'/tsne/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/tsne/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/tsne/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/tsne/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/tsne/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/tsne/sample_{sample}_de{de_prob}loc_{loc}.err'
     shell:
         "python scripts/apply_tsne.py {input} {output} {params.n_components} {params.init}  2> {log.err} 1> {log.out}"
 
 rule phenograph:
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         n_neighbours = config['clustering']['phenograph']['n_neighbours']
     output:
-        ANALYSIS_OUTPUT+'/phenograph/clusters/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/phenograph/clusters/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/phenograph/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/phenograph/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/phenograph/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/phenograph/sample_{sample}_de{de_prob}loc_{loc}.err'
     threads: 8
     shell:
         "python scripts/apply_phenograph.py {input} {output} {params.n_neighbours} --n_threads {threads} 2> {log.err} 1> {log.out}"
 
 rule griph:
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         #TODO read from the config
     output:
-        ANALYSIS_OUTPUT+'/griph/clusters/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/griph/clusters/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/griph/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/griph/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/griph/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/griph/sample_{sample}_de{de_prob}loc_{loc}.err'
     threads:8
     shell:
         "Rscript scripts/apply_griph.R --input {input} --output {output} --threads {threads} 2> {log.err} 1> {log.out}"
 
 
 rule zifa:
-    # by default n_blocks are estimated as n_cells/500 
+    # by default n_blocks are estimated as n_cells/500
     input:
-        SIMULATED_DATA_OUTPUT+'/{sample}_sim_loc{loc}_zheng17.h5'
+        SIMULATED_DATA_OUTPUT+'/{sample}_sim_de{de_prob}_loc{loc}_zheng17.h5'
     params:
         n_components = config['dim_reduction']['block_zifa']['n_components'],
         n_blocks = config['dim_reduction']['block_zifa']['n_blocks']
     output:
-        ANALYSIS_OUTPUT+'/block_zifa/{sample}_sim_loc'+'{loc}'+'.csv'
+        ANALYSIS_OUTPUT+'/block_zifa/{sample}_sim_de{de_prob}_loc'+'{loc}'+'.csv'
     log:
-        out = LOG_FILES+'/zifa/sample_{sample}loc_{loc}.out',
-        err = LOG_FILES+'/zifa/sample_{sample}loc_{loc}.err'
+        out = LOG_FILES+'/zifa/sample_{sample}_de{de_prob}loc_{loc}.out',
+        err = LOG_FILES+'/zifa/sample_{sample}_de{de_prob}loc_{loc}.err'
     shell:
         "python scripts/apply_zifa.py {input} {output} {params.n_components} --n_blocks {params.n_blocks} 1> {log.out} 2> {log.err}"
-
-
-
-
-
