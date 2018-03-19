958ac8d001ac10286d304a5392262a990d260ed4
Mustafa Anil Tuncel <tuncel.manil@gmail.com>
2018-03-19 15:34:55 +0100

    filtering out non-detected genes

--- scripts/create_hdf5.py
+++ scripts/create_hdf5.py
@@ -17,11 +17,19 @@ args = parser.parse_args()
 def cellranger_to_hdf5 (genes, matrix_file, barcodes, out_path):
     # to read a matrix market file
     matrix = mmread(matrix_file.__str__()).astype("float32").todense().T
+    matrix = np.asarray(matrix)
     
     gene_ids = np.genfromtxt(genes.__str__(), dtype='S16')[:,0]
     gene_names = np.genfromtxt(genes.__str__(), dtype='S16')[:, 1]
     cell_names = np.genfromtxt(barcodes.__str__(), dtype='S16')
-   
+
+    # removing all-zero-genes accross all cells
+    detected_genes_index = ~(matrix == 0).all(axis=0) 
+    matrix = matrix[:,detected_genes_index]
+    gene_ids = gene_ids[detected_genes_index]
+    gene_names = gene_names[detected_genes_index]
+
+
     f = h5py.File(out_path.__str__(), "w")
     f.create_dataset(name = 'matrix', data = matrix)
     gg = f.create_group('gene_attrs')
