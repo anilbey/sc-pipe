0bc6f53e5ad2c383073e0473a4aebd5ec7742cac
Tuncel  Mustafa Anil <tuncel.manil@gmail.com>
2018-03-25 16:48:25 +0200

    added test_data directory

--- unit_tests/test_create_hdf5.py
+++ unit_tests/test_create_hdf5.py
@@ -12,9 +12,9 @@ sys.path.append(scripts_path)
 from utils import cellranger_to_hdf5
 
 # test data paths
-pbmc3k = os.path.join(dirname, '../data/pbmc3k/filtered_gene_bc_matrices/hg19')
+pbmc3k = os.path.join(dirname, '../data/pbmc3k/filtered_gene_bc_matrices/hg19/')
 pbmc3k_out = os.path.join(dirname, './test_data/pbmc3k.hdf5')
-
+logging.debug('out_file_path: ' + pbmc3k_out)
 
 @pytest.mark.parametrize("genes_file, matrix_file, barcodes_file, out_path", [(pbmc3k + 'genes.tsv', pbmc3k + 'matrix.mtx', pbmc3k + 'barcodes.tsv', pbmc3k_out)])
 def test_cellranger_to_hdf5(genes_file, matrix_file, barcodes_file, out_path):
