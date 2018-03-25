b1fefaa8eb4313553ab27636143410b23acb35f0
Mustafa Anil Tuncel <tuncel.manil@gmail.com>
2018-03-25 16:32:02 +0200

    fixes the relative path issue

--- unit_tests/test_create_hdf5.py
+++ unit_tests/test_create_hdf5.py
@@ -1,15 +1,23 @@
 import pytest
 import sys
-import os.path
-sys.path.append("../scripts")
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+dirname = os.path.dirname(__file__)
+logging.debug('__file__ variable: ' +  __file__)
+logging.debug('dirname: ' + dirname)
+scripts_path = os.path.join(dirname, '../scripts')
+sys.path.append(scripts_path)
 from utils import cellranger_to_hdf5
 
 # test data paths
-pbmc3k = '../data/pbmc3k/filtered_gene_bc_matrices/hg19'
-pbmc3k_out = './test_data/pbmc3k.hdf5'
+pbmc3k = os.path.join(dirname, '../data/pbmc3k/filtered_gene_bc_matrices/hg19')
+pbmc3k_out = os.path.join(dirname, './test_data/pbmc3k.hdf5')
 
 
 @pytest.mark.parametrize("genes_file, matrix_file, barcodes_file, out_path", [(pbmc3k + 'genes.tsv', pbmc3k + 'matrix.mtx', pbmc3k + 'barcodes.tsv', pbmc3k_out)])
 def test_cellranger_to_hdf5(genes_file, matrix_file, barcodes_file, out_path):
     cellranger_to_hdf5(genes_file, matrix_file, barcodes_file, out_path)
-    assert os.path.isfile(out_path) 
+    assert os.path.isfile(out_path)
+
